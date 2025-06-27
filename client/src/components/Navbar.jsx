7655ff09d9db82c7ad5e09f62d79a3b966e527cf
Er.AmitBehera_AKB <amit.behera2428@gmail.com>
2025-06-28 01:07:41 +0530

    Set up React frontend using Vite and added base layout

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{ backgroundColor: '#00704A', padding: '1rem', color: '#fff' }}>
+      <h2>AgriFlow</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
